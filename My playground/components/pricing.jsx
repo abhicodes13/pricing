d8b29bf6e5bba4d9e89308a36daf40088d449878
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-04-04 18:59:36 -0400

    Pushed all files in pricing repo

---
+++ My playground/components/pricing.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Pricing(){
+    return (
+        <div className="pricing">
+        <div className="price-card">
+        <h1 className="mnth">Monthly</h1>
+        <h3><s>100</s></h3>
+        <h2>$45/mo</h2>
+        <p>Enjoy access to all the chatrooms       of this group </p>
+        <button className="btn">Buy Now</button>
+        </div>
+         <div className="price-card">
+        <h1 className="mnth">Annual</h1>
+        <h3><s>540</s></h3>
+        <h2>430/year</h2>
+        <p>Enjoy access to all the chatrooms       of this group </p>
+                <button className="btn">Buy Now</button>
+        </div>
+         <div className="price-card">
+        <h1 className="mnth">Lifetime</h1>
+       <h3><s>100</s></h3>
+        <h2>$1000</h2>
+        <p>Enjoy access to all the chatrooms       of this group </p>
+                <button className="btn">Buy Now</button>
+        </div>
+        </div>
+        
+    )
+}
