6def87ed2add4240a0adc92e7308f84c2e2c17cc
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-04-04 19:13:10 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "../components/header";
+import Pricing from "../components/pricing";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Pricing/>
+    </div>
+  );
+}
+
+export default App;
