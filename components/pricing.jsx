21361c6b6d28ac4acd52db9b47a87ed6a1e95ad1
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-04-07 18:51:04 -0400

    Update pricing.jsx

--- components/pricing.jsx
+++ components/pricing.jsx
@@ -7,21 +7,21 @@ export default function Pricing(){
         <h1 className="mnth">Monthly</h1>
         <h3><s>100</s></h3>
         <h2>$45/mo</h2>
-        <p>Enjoy access to all the chatrooms       of this group </p>
+        <p>Enjoy access to all the chatrooms of this group </p>
         <button className="btn">Buy Now</button>
         </div>
          <div className="price-card">
         <h1 className="mnth">Annual</h1>
         <h3><s>540</s></h3>
         <h2>430/year</h2>
-        <p>Enjoy access to all the chatrooms       of this group </p>
+        <p>Enjoy access to all the chatrooms of this group </p>
                 <button className="btn">Buy Now</button>
         </div>
          <div className="price-card">
         <h1 className="mnth">Lifetime</h1>
        <h3><s>100</s></h3>
         <h2>$1000</h2>
-        <p>Enjoy access to all the chatrooms       of this group </p>
+        <p>Enjoy access to all the chatrooms of this group </p>
                 <button className="btn">Buy Now</button>
         </div>
         </div>
